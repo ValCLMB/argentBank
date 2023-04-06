3d6865d33003c50fbdac3fb2b385f184024b7a03
tenma <tenma@osxt.com>
2023-04-06 11:16:49 +0200

    create header and footer components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import "./Header.css";
+export const Header = () => {
+    return (
+        <nav className="main-nav">
+            <a className="main-nav-logo" href="./index.html">
+                <img
+                    className="main-nav-logo-image"
+                    src="./img/argentBankLogo.png"
+                    alt="Argent Bank Logo"
+                />
+                <h1 className="sr-only">Argent Bank</h1>
+            </a>
+            <div>
+                <a className="main-nav-item" href="./sign-in.html">
+                    <FontAwesomeIcon icon={faUserCircle} />
+                    Sign In
+                </a>
+            </div>
+        </nav>
+    );
+};
