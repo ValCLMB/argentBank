0da3ed96fb1b0b9a3147aa7d51ab1b5073d75de4
tenma <tenma@osxt.com>
2023-04-06 11:22:33 +0200

    add home container

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,11 +1,14 @@
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
 import "./App.css";
+import { Home } from "./Home/Home";
 
 function App() {
+    console.log(window.location);
     return (
         <div className="App">
             <Header />
+            {window.location.pathname === "/" && <Home />}
             <Footer />
         </div>
     );
