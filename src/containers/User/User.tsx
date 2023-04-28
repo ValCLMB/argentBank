60ba956990c80aa278061902885c78b3e531bfd3
tenma <tenma@osxt.com>
2023-04-28 09:38:32 +0200

    add edit user

--- src/containers/User/User.tsx
+++ src/containers/User/User.tsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Account } from "../../components/Account/Account";
 import "./User.css";
 import { RootState } from "../../store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { addUser } from "../../features/userSlice";
 
 const accounts = [
@@ -25,7 +25,34 @@ const accounts = [
 export const User = () => {
     const dispatch = useDispatch();
     const { user } = useSelector((state: RootState) => state);
+    const [isEdit, setIsEdit] = useState(false);
 
+    const editUser = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const target = e.target as HTMLFormElement;
+
+        const { firstName, lastName } = target;
+        const data = { firstName: firstName.value, lastName: lastName.value };
+
+        fetch(import.meta.env.VITE_API + "/user/profile", {
+            method: "PUT",
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                if (res.status !== 200) {
+                    throw new Error(res.message);
+                }
+                setIsEdit(false);
+            });
+    };
+
+    // GET USER
     useEffect(() => {
         const abortController = new AbortController();
         const signal = abortController.signal;
@@ -40,22 +67,62 @@ export const User = () => {
         })
             .then((res) => res.json())
             .then((res) => {
-                res.status === 200
-                    ? dispatch(addUser(res.body))
-                    : console.log("error");
+                if (res.status === 200) {
+                    dispatch(addUser(res.body));
+                } else {
+                    window.location.pathname = "/";
+                    throw new Error(res.message);
+                }
             });
 
         return () => abortController.abort();
-    }, []);
+    }, [editUser]);
+
     return (
         <main className="main bg-dark">
             <div className="header">
                 <h1>
                     Welcome back
                     <br />
-                    Tony Jarvis!
+                    {/* NAME DISPLAY / INPUT */}
+                    {isEdit ? (
+                        <form onSubmit={editUser}>
+                            <input
+                                type="text"
+                                id="firstName"
+                                defaultValue={user.firstName}
+                            />
+                            <input
+                                type="text"
+                                id="lastName"
+                                defaultValue={user.lastName}
+                            />
+                            <div>
+                                <button className="edit-button" type="submit">
+                                    Save
+                                </button>
+                                <button
+                                    className="edit-button"
+                                    onClick={() => setIsEdit(!isEdit)}
+                                >
+                                    Cancel
+                                </button>
+                            </div>
+                        </form>
+                    ) : (
+                        <>
+                            {user.firstName} {user.lastName}
+                            <div>
+                                <button
+                                    className="edit-button"
+                                    onClick={() => setIsEdit(!isEdit)}
+                                >
+                                    Edit Name
+                                </button>
+                            </div>
+                        </>
+                    )}
                 </h1>
-                <button className="edit-button">Edit Name</button>
             </div>
             {accounts.map((account) => (
                 <Account
